c87eb8a66cd9b101e2c6539ebe5f130096526cf9
Faraz Ahmed <farazfatahsoomro@gmail.com>
2026-01-15 19:42:28 +0500

    Added Guest Reservation and ReservePayer

---
+++ SRC/ReservePayer.java
@@ -0,0 +1,13 @@
+public class ReservePayer {
+    private final CreditCard creditCardDetails;
+    private final Identity id;
+
+    public ReservePayer(CreditCard card, Identity id) {
+        this.creditCardDetails = card;
+        this.id = id;
+    }
+
+    public static ReservePayer create(CreditCard card, Identity id) {
+        return new ReservePayer(card, id);
+    }
+}
